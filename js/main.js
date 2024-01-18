2784f79e76b42c6b2039a33e516366e2badbbe65
ALESSANDRO CAPILUPPI <115798135+picodesign@users.noreply.github.com>
2024-01-18 17:13:37 +0100

    Avancement - responsive home and better rest css

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
 //script pour filtres
-const filterButtons = document.querySelectorAll('.filtre');
-const travaux = document.querySelectorAll('.travail');
+const filterButtons = document.querySelectorAll(".filtre");
+const travaux = document.querySelectorAll(".travail");
 
-filterButtons.forEach(button => {
-  button.addEventListener('click', () => {
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
     const selectedTag = button.dataset.filter;
-    const isFilterActive = button.classList.contains('active');
+    const isFilterActive = button.classList.contains("active");
 
     if (isFilterActive) {
-      travaux.forEach(travail => {
-        travail.style.display = 'block';
+      travaux.forEach((travail) => {
+        travail.style.display = "block";
       });
-      button.classList.remove('active');
+      button.classList.remove("active");
     } else {
-      travaux.forEach(travail => {
-        const tags = travail.querySelector('a').dataset.tags.split(',').map(tag => tag.trim());
+      travaux.forEach((travail) => {
+        const tags = travail
+          .querySelector("a")
+          .dataset.tags.split(",")
+          .map((tag) => tag.trim());
         if (tags.includes(selectedTag)) {
-          travail.style.display = 'block';
+          travail.style.display = "block";
         } else {
-          travail.style.display = 'none';
+          travail.style.display = "none";
         }
       });
-      filterButtons.forEach(btn => btn.classList.remove('active'));
-      button.classList.add('active');
+      filterButtons.forEach((btn) => btn.classList.remove("active"));
+      button.classList.add("active");
     }
   });
 });
-//fin script filtre
