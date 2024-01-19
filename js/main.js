ca5bf040c4b16361f2c6a7d0f603d06f79cf519e
ALESSANDRO CAPILUPPI <115798135+picodesign@users.noreply.github.com>
2024-01-19 21:59:28 +0100

    Ajout
    J'ai ajouté l'animation keyframes et créé un script JS pour l'activer en hiver. J'ai également activé les différents thèmes en fonction de l'heure locale et effectué des corrections de CSS et de couleurs.

--- js/main.js
+++ js/main.js
@@ -36,3 +36,41 @@ document.getElementById('check').addEventListener('change', function() {
   document.body.classList.toggle('active', this.checked);
 });
 
+
+// thème sombre-noir par rapport a lheure
+function setTheme() {
+  const now = new Date();
+  const hours = now.getHours();
+
+  const isDaytime = hours >= 8 && hours < 18;
+
+  const root = document.documentElement;
+  root.style.setProperty('--text', isDaytime ? '#1c1917' : '#E8E5E3');
+  root.style.setProperty('--background', isDaytime ? '#efebeb' : '#141010');
+  root.style.setProperty('--primary', isDaytime ? '#524a42' : '#BDB5AD');
+  root.style.setProperty('--secondary', isDaytime ? '#b2b4a2' : '#5b5d4b');
+  root.style.setProperty('--accent', isDaytime ? '#6f775f' : '#98a088');
+}
+window.onload = setTheme;
+// fin thèmes
+
+// Animation css visble seulement en hiver
+document.addEventListener("DOMContentLoaded", function () {
+  // Récupérer le mois actuel
+  const moisActuel = new Date().getMonth() + 1; // Les mois commencent à partir de 0
+
+  if (moisActuel >= 12 || moisActuel <= 2) {
+      // Afficher les div
+      const snowDiv = document.querySelector('.snow');
+      snowDiv.style.display = 'block';
+      const calques = document.querySelectorAll('.snow__calque');
+      calques.forEach(calque => {
+          calque.style.display = 'block';
+      });
+      console.log("Nous sommes en hiver. Affichage des éléments.");
+
+  } else{
+    console.log("Nous ne sommes pas en hiver. Les éléments restent masqués.");
+  }
+});
+// fin animation automatique
