b71d2c7325b79ec28fae493d4668ecce8f7743e0
ALESSANDRO CAPILUPPI <115798135+picodesign@users.noreply.github.com>
2024-03-06 16:15:49 +0100

    Yuvpage
    J'ai fini avec Yuv. J'ai également corrigé quelques bugs, mais il semble y avoir un problème avec les téléphones sur la page "À propos" en mode sombre, je crois. Il faudrait tester si les vidéos fonctionnent sur mobile.

--- php/page_protégée_yuv.php
+++ php/page_protégée_yuv.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Vérifier si l'utilisateur est connecté, le rediriger vers la page de connexion s'il ne l'est pas
-if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header("location: login_yuv.php");
     exit;
 }
@@ -17,8 +17,8 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/work/yuv.css">
-    <script src="/js/main.js" defer></script>
-    <script src="/js/app.js" defer></script>
+    <script src="../js/main.js" defer></script>
+    <script src="../js/app.js" defer></script>
     <title>Yuv</title>
 </head>
 
@@ -32,11 +32,8 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
             <li class="lien"><a href="/php/page_protégée_yuv.php">FR</a></li>
             <li class="lien"><a href="/php/page_protégée_yuv.php">IT</a></li>
         </ul>
-        <div class="titre">Yuv</div>
-        <video class="image" control="false" autoplay="autoplay" loop="true">
-            <source type="video/webm" src="/images/yuv/timelapse.webm" />
-            <source type="video/mp4" src="/images/yuv/timelapse.mp4" />
-        </video>
+        <div class="titre">Yuv beauty</div>
+        <img class="image" src="/images/yuv/front.jpg"></img>
         <div class="colab">
             Collaboration avec
             <span><a href="https://www.grapheine.com">Graphéine</a> pour <a href="https://www.yuv.co">Yuv</a></span>
@@ -45,83 +42,169 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
     </div>
 
     <!-- Contenu en dessous de la ligne de flottaison -->
+    <!-- partie 1 - intro contexte et existant-->
     <div class="description">
         <h5>Contexte</h5>
-        <p>Pour completer la charte graphique designer par Graphéine, j'ai eté charger de devlopper plus en approfondi quelques idée pour les motion de la marque.</p>
-        <p>Yuv propose une solution innovante pour la coloration professionnelle des cheveux. Grâce à son système numérique ca permet de formuler une infinité de nuances adaptées aux besoins de chaque client.</p>
-        <p> Contrairement aux tubes jetables traditionnels, les cartouches d'aluminium Yuv sont conçues dans une logique d'économie circulaire: elles sont vidées puis rechargées de nouveau, éliminant ainsi les déchets plastiques à usage unique.</p>
+        <h6>Pour compléter la charte graphique conçue par Graphéine, j'ai été chargé d'approfondir quelques idées pour les animations de la marque.</h6>
+        <p>Yuv propose une solution innovante pour la coloration professionnelle des cheveux. Son système numérique permet de formuler une multitude de nuances adaptées aux besoins de chaque client.</p>
+        <p>Contrairement aux tubes jetables traditionnels, les cartouches d'aluminium de Yuv sont conçues dans une logique d'économie circulaire : elles sont vidées puis rechargées, éliminant ainsi les déchets plastiques à usage unique.</p>
     </div>
     <div class="below-fold">
-
-
-
-        <!-- Galerie d'images -->
-        <div class="gallery">
-            <div class="double-container">
-                <div id="player"></div>
-                <!-- <script src="https://www.youtube.com/iframe_api"></script>
-                <script>
-                    let player;
-
-                    function onYouTubeIframeAPIReady() {
-                        player = new YT.Player('player', {
-                            height: '600px',
-                            videoId: 'NLuGfE-3aaM',
-                            playerVars: {
-                                'autoplay': 1,
-                                'controls': 1,
-                                'rel': 0,
-                                'modestbranding': 1,
-                                'loop': 1,
-                                'fs': 1,
-                                'iv_load_policy': 3,
-                                'start': 105,
-                                'enablejsapi': 1,
-                                'origin': window.location.origin
-                            },
-                            events: {
-                                'onReady': onPlayerReady
-                            }
-                        });
-                    }
-
-                    function onPlayerReady(event) {
-                        // volume range
-                        // player.setVolume(20);
-                        player.setVolume(2);
-                    }
-                </script> -->
-            </div>
+        <div class="gallery_mid2">
             <div class="image-container">
-                <video autoplay loop muted class="gallery-image">
-                    <source type="video/webm" src="/images/vm/backstage_timelapse.webm" />
-                    <source type="video/mp4" src="/images/vm/backstage_timelapse.mp4" />
-                </video>
+                <img class="gallery-image" src="/images/yuv/chargement_machine.jpg"></img>
                 <div class="image-title"></div>
             </div>
             <div class="image-container">
-                <video autoplay loop muted class="gallery-image">
-                    <source type="video/webm" src="/images/vm/backstage_timelapse_2.webm" />
-                    <source type="video/mp4" src="/images/vm/backstage_timelapse_2.mp4" />
-                </video>
+                <img class="gallery-image" src="/images/yuv/cartouches.jpg"></img>
                 <div class="image-title"></div>
             </div>
             <div class="image-container">
-                <video autoplay loop muted class="gallery-image">
-                    <source type="video/webm" src="/images/vm/backstage_timelapse_3.webm" />
-                    <source type="video/mp4" src="/images/vm/backstage_timelapse_3.mp4" />
-                </video>
+                <img class="gallery-image" src="/images/yuv/app_existant.jpg"></img>
                 <div class="image-title"></div>
             </div>
             <div class="image-container">
-                    <img class="gallery-image" src="/images/vm/projection.jpg"></img>
-                    <div class="image-title"></div>
-            </div>
-
+                <img class="gallery-image" src="/images/yuv/reference_motionexistant.jpg"></img>
+                <div class="image-title"></div>
             </div>
         </div>
-    <a href="logout_yuv.php">Se déconnecter</a>
 
+    </div>
+    <!-- fin -->
+    <!-- partie 2 Debut Motion -->
+    <div class="description">
+        <h5>Motion 3D</h5>
+        <h6>J'ai débuté par la création d'une vidéo promotionnelle visant à lier l'utilisation du Motion existant avec la machine.</h6>
+        <p>J'ai décidé d'associer les points aux cartouches pour mon animation. En positionnant la caméra au-dessus de la machine dans Cinema 4D, j'ai obtenu ce résultat minimaliste. Ensuite, grâce à une transition en fondu, j'ai pu reproduire le mouvement existant dans After Effects.</p>
+    </div>
+
+    <!-- video et images -->
+    <div class="below-fold">
+        <div class="gallery2">
+            <div class="double-container">
+                <video controls loop muted class="gallery-image">
+                    <source type="video/webm" src="/images/yuv/Final_3D.webm" />
+                    <source type="video/mp4" src="/images/yuv/Final_3D.mp4" />
+                </video>
+                <div class="image-title"></div>
+            </div>
+            <div class="image-container">
+                <img class="gallery-image" src="/images/yuv/plan_intro.jpg"></img>
+                <div class="image-title"></div>
+            </div>
+
+            <div class="image-container">
+                <img class="gallery-image" src="/images/yuv/plan_cartouches.jpg"></img>
+                <div class="image-title"></div>
+            </div>
+
+            <div class="image-container">
+                <img class="gallery-image" src="/images/yuv/plan_macro.jpg"></img>
+                <div class="image-title"></div>
+            </div>
+
+            <div class="image-container">
+                <img class="gallery-image" src="/images/yuv/plan_haut.jpg"></img>
+                <div class="image-title"></div>
+            </div>
+        </div>
+    </div>
+    <!-- fin -->
+    <!-- partie 3  Ae motion-->
+    <div class="description">
+        <h5>Motion texte</h5>
+        <h6>J'ai exploré divers concepts d'animation en suivant la charte graphique de Graphéine.</h6>
+        <p>Avec les retours de l'équipe, j'ai progressivement affiné ce motion mettant en valeur les avantages de la marque.</p>
+        <p>Un cylindre est utilisé dans l'animation pour symboliser la machine et les cartouches (sous forme de texte), soulignant ainsi les atouts de la marque.</p>
+    </div>
+    <!-- video -->
+    <div class="below-fold">
+        <div class="gallery_mid2">
+            <div class="double-container">
+                <video controls loop muted class="gallery-image">
+                    <source type="video/webm" src="/images/yuv/Final_Ae.webm" />
+                    <source type="video/mp4" src="/images/yuv/Final_Ae.mp4" />
+                </video>
+                <div class="image-title"></div>
+            </div>
+            <div class="image-container">
+                <video autoplay loop muted class="gallery-image">
+                    <source type="video/webm" src="/images/yuv/concept_gradient_hair.webm" />
+                    <source type="video/mp4" src="/images/yuv/concept_gradient_hair.mp4" />
+                </video>
+                <div class="image-title"></div>
+            </div>
+
+            <div class="image-container">
+                <video autoplay loop muted class="gallery-image">
+                    <source type="video/webm" src="/images/yuv/concept_draw_logo.webm" />
+                    <source type="video/mp4" src="/images/yuv/concept_draw_logo.mp4" />
+                </video>
+                <div class="image-title"></div>
+            </div>
+
+            <div class="image-container">
+                <video autoplay loop muted class="gallery-image">
+                    <source type="video/webm" src="/images/yuv/concept_draw_color.webm" />
+                    <source type="video/mp4" src="/images/yuv/concept_draw_color.mp4" />
+                </video>
+                <div class="image-title"></div>
+            </div>
+
+            <div class="image-container">
+                <video autoplay loop muted class="gallery-image">
+                    <source type="video/webm" src="/images/yuv/concept_fill.webm" />
+                    <source type="video/mp4" src="/images/yuv/concept_fill.mp4" />
+                </video>
+                <div class="image-title"></div>
+            </div>
+            <div class="image-container">
+                <video autoplay loop muted class="gallery-image">
+                    <source type="video/webm" src="/images/yuv/concept_doted_white.webm" />
+                    <source type="video/mp4" src="/images/yuv/concept_doted_white.mp4" />
+                </video>
+                <div class="image-title"></div>
+            </div>
+            <div class="image-container">
+                <video autoplay loop muted class="gallery-image">
+                    <source type="video/webm" src="/images/yuv/concept_doted_black.webm" />
+                    <source type="video/mp4" src="/images/yuv/concept_doted_black.mp4" />
+                </video>
+                <div class="image-title"></div>
+            </div>
+        </div>
+    </div>
+    <!-- fin -->
+    <!-- Pictos -->
+    <div class="description">
+        <h5>Bonus - Pictos</h5>
+        <h6>Vers la fin de mon stage, l'équipe de Graphéine envisageait de produire une série de pictogrammes pour servir aux futurs modes d'emploi de la machine.</h6>
+        <p>Disposant d'un modèle 3D assez fidèle de la machine pour mes animations, j'ai initié les vues isométriques pour l'illustratrice de l'équipe.</p>
+    </div>
+    <div class="below-fold">
+        <div class="gallery_mid">
+            <div class="image-container">
+                <img class="gallery-image" src="/images/yuv/3D_object.jpg"></img>
+                <div class="image-title"></div>
+            </div>
+            <div class="image-container">
+                <img class="gallery-image" src="/images/yuv/pictos_base.jpg"></img>
+                <div class="image-title"></div>
+            </div>
+            <div class="image-container">
+                <img class="gallery-image" src="/images/yuv/asset_pack_yuv.jpg"></img>
+                <div class="image-title"></div>
+            </div>
+            <div class="image-container">
+                <img class="gallery-image" src="/images/yuv/cartouches_render.jpg"></img>
+                <div class="image-title"></div>
+            </div>
+        </div>
+
+    </div>
+    <!-- fin -->
+    <!-- footer deconection -->
+    <div class="logout"><a href="logout_yuv.php">Se déconnecter</a></div>
 </body>
 
 </html>
