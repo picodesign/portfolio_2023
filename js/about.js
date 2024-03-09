dda2166cad3fd52ea3699347d474729ee22eb47c
ALESSANDRO CAPILUPPI <115798135+picodesign@users.noreply.github.com>
2024-03-09 18:41:56 +0100

    mis a jour des pages et grosse corrections
    Le mode sombre a été corrigé : désormais, chaque utilisateur peut choisir le thème qui lui convient, et la page "À propos" a été améliorée

--- js/about.js
+++ js/about.js
@@ -45,6 +45,16 @@ gsap.from(".row > .col",2, {
   }
 });
 
+gsap.from(".message",2, {
+  opacity:0,
+  y:40,
+  ease:"power3.inOut",
+  delay:2.5,
+  stagger:{
+    amount:1,
+  }
+});
+
 gsap.from(".marquee", 1, {
   bottom: "-20%", 
   opacity: 0,
--- js/main.js
+++ js/main.js
@@ -5,41 +5,23 @@ window.addEventListener("resize", () => {
 });
 // fin
 
-// video switch
+// changement video en  fonction du theme
 function setThemeAndVideo() {
-  // console.log('La fonction setThemeAndVideo est exécutée.');
-
-  const now = new Date();
-  const hours = now.getHours();
-  // console.log('Heures actuelles :', hours);
-
-  const isDaytime = hours >= 8 && hours < 19;
-  // console.log('Est-ce la journée ?', isDaytime);
-
-  const root = document.documentElement;
-  root.style.setProperty("--text", isDaytime ? "#1c1917" : "#E8E5E3");
-  root.style.setProperty("--background", isDaytime ? "#efebeb" : "#141010");
-  root.style.setProperty("--primary", isDaytime ? "#524a42" : "#BDB5AD");
-  root.style.setProperty("--secondary", isDaytime ? "#b2b4a2" : "#5b5d4b");
-  root.style.setProperty("--accent", isDaytime ? "#6f775f" : "#98a088");
-
+  const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
   const lightVideo = document.getElementById("lightVideo");
   const darkVideo = document.getElementById("darkVideo");
 
-  if (isDaytime) {
-    // console.log('C\'est la journée. Affichage de la vidéo "light".');
-    lightVideo.style.display = "block";
-    darkVideo.style.display = "none";
-  } else {
-    // console.log('C\'est la nuit. Affichage de la vidéo "dark".');
+  if (isDarkMode) {
     lightVideo.style.display = "none";
     darkVideo.style.display = "block";
+  } else {
+    lightVideo.style.display = "block";
+    darkVideo.style.display = "none";
   }
 
   lightVideo.play();
   darkVideo.play();
 }
-
 window.addEventListener("load", setThemeAndVideo);
 // fin
 
@@ -81,21 +63,21 @@ document.getElementById("check").addEventListener("change", function () {
 });
 
 // Animation css visble seulement en hiver
-document.addEventListener("DOMContentLoaded", function () {
-  // Récupérer le mois actuel
-  const moisActuel = new Date().getMonth() + 1; // Les mois commencent à partir de 0
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Récupérer le mois actuel
+//   const moisActuel = new Date().getMonth() + 1; // Les mois commencent à partir de 0
 
-  if (moisActuel >= 12 || moisActuel <= 2) {
-    // Afficher les div
-    const snowDiv = document.querySelector(".snow");
-    snowDiv.style.display = "block";
-    const calques = document.querySelectorAll(".snow__calque");
-    calques.forEach((calque) => {
-      calque.style.display = "block";
-    });
-    console.log("Nous sommes en hiver. Affichage des éléments.");
-  } else {
-    console.log("Nous ne sommes pas en hiver. Les éléments restent masqués.");
-  }
-});
+//   if (moisActuel >= 12 || moisActuel <= 2) {
+//     // Afficher les div
+//     const snowDiv = document.querySelector(".snow");
+//     snowDiv.style.display = "block";
+//     const calques = document.querySelectorAll(".snow__calque");
+//     calques.forEach((calque) => {
+//       calque.style.display = "block";
+//     });
+//     console.log("Nous sommes en hiver. Affichage des éléments.");
+//   } else {
+//     console.log("Nous ne sommes pas en hiver. Les éléments restent masqués.");
+//   }
+// });
 // fin animation automatique
